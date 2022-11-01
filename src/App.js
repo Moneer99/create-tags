4fb48cdcb635adfdfe49dbe099ba486752bd5b9d
moneer99 <moneerbinoon9@gmail.com>
2022-11-01 23:20:35 +0300

    add logic for create tags

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [addTags, setAddTagas] = useState([]);
+
+  const createTags = (e) => {
+    //setAddTagas([...addTags, e.target.value])
+    if (e.key === "Enter") {
+      setAddTagas([...addTags, e.target.value]);
+      // to remove a create tags on click enter
+      e.target.value = "";
+    }
+  };
   return (
     <div className="App">
       <h1>hello</h1>
